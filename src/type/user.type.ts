e743a0846f9a62e378ccad8dba340a833d2a7ce8
tzptzptzptzptzp <git@tzp.pink>
2023-12-22 01:39:10 +0900

    🏷️ ユーザーの型定義ファイルを作成

---
+++ src/type/user.type.ts
@@ -0,0 +1,31 @@
+type Geo = {
+  lat: string;
+  lng: string;
+};
+
+type Address = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+};
+
+type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+export type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+};
+
+export type UsersType = UserType[];
