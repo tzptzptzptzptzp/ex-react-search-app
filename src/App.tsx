a12e7d8cf933bc681055e2a8ed9c7e619b6ee074
tzptzptzptzptzp <git@tzp.pink>
2023-12-21 23:44:57 +0900

    ✨ 雛形を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <div>React</div>;
+  return (
+    <div className="App">
+      <div className="main">
+        <h2>React Search App</h2>
+        <input type="text" />
+        <div className="content">
+          <div className="box">
+            <h3>UserName</h3>
+            <hr />
+            <p>MailAddress</p>
+          </div>
+          <div className="box">
+            <h3>UserName</h3>
+            <hr />
+            <p>MailAddress</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
