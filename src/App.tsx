c786be718a898631208607192a937945f93543fb
tzptzptzptzptzp <git@tzp.pink>
2023-12-25 00:16:32 +0900

    ✨ フィルター機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { UserType } from "./type/user.type";
 
 import "./App.css";
@@ -6,7 +6,10 @@ import "./App.css";
 const API_URL = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
+  const ref = useRef<HTMLInputElement>(null);
+
   const [users, setUsers] = useState([]);
+  const [seacthQuery, setSearchQuery] = useState([]);
 
   useEffect(() => {
     fetch(API_URL)
@@ -18,15 +21,29 @@ function App() {
       });
   }, []);
 
+  const handleSearch = () => {
+    const searchTerm = ref.current?.value || "";
+    setSearchQuery(
+      users.filter((user: UserType) =>
+        user.name.toLowerCase().includes(searchTerm)
+      )
+    );
+  };
+
   return (
     <div className="text-center box-border">
       <div className="">
         <div className="mt-16 mb-8 text-5xl">
           <h2>React Search App</h2>
-          <input className="h-6 px-1 py-3 border text-lg" type="text" />
+          <input
+            className="h-6 px-1 py-3 border text-lg"
+            onChange={() => handleSearch()}
+            ref={ref}
+            type="text"
+          />
         </div>
         <div className="grid grid-cols-5 gap-x-4 gap-y-12 w-[1200px] mx-auto">
-          {users.map((user: UserType) => (
+          {seacthQuery.map((user: UserType) => (
             <div className="w-full min-h-[12rem] p-4 rounded-lg shadow-lg">
               <div className="flex items-center justify-center h-20">
                 <h3 className="text-[1.5rem]">{user.name}</h3>
